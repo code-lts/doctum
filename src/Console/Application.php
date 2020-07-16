5fe0a46e5efded0c12e7b3e94a54795e86b6a3f1
William Desportes <williamdes@wdes.fr>
2020-07-16 21:08:48 +0200

    chore: update copyrights

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -34,6 +34,6 @@ class Application extends BaseApplication
 
     public function getLongVersion()
     {
-        return parent::getLongVersion() . ' by <comment>Fabien Potencier</comment>';
+        return parent::getLongVersion() . ' by <comment>Fabien Potencier and William Desportes</comment>';
     }
 }
