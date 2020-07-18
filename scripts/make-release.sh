f72ce4d46e238e0ad1adba42f60aac465aaec9b0
William Desportes <williamdes@wdes.fr>
2020-07-18 23:28:04 +0200

    feat: sign phar releases

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -14,7 +14,14 @@ echo "${VERSION}" > ./build/VERSION
 
 echo "Release for : ${VERSION}"
 
+GPG_KEY=${GPG_KEY:-C4D91FDFCEF6B4A3C653FD7890A0EF1B8251A889}
+
 composer update --no-dev
 php -dphar.readonly=0 ./scripts/phar-generator-script.php
 composer update
 cp CHANGELOG.md ./build/
+sha256sum ./build/doctum.phar > ./build/doctum.phar.sha256
+sha256sum ./build/* > ./build/files.sha256
+gpg --detach-sig --local-user "${GPG_KEY}" --armor ./build/doctum.phar
+gpg --detach-sig --local-user "${GPG_KEY}" --armor ./build/doctum.phar.sha256
+gpg --detach-sig --local-user "${GPG_KEY}" --armor ./build/files.sha256
