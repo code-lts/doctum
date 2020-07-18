afaeb50ddcdd178d8b05602b550d35f201849cc9
William Desportes <williamdes@wdes.fr>
2020-07-18 23:05:08 +0200

    tests: cover Doctum\RemoteRepository\*

---
+++ tests/RemoteRepositoryTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Doctum\Tests;
+
+use Doctum\RemoteRepository\BitBucketRemoteRepository;
+use Doctum\RemoteRepository\GitHubRemoteRepository;
+use Doctum\RemoteRepository\GitLabRemoteRepository;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author William Desportes <williamdes@wdes.fr>
+ */
+class RemoteRepositoryTest extends TestCase
+{
+    /**
+     * Test for BitBucketRemoteRepository class
+     */
+    public function testBitBucketRemoteRepository(): void
+    {
+        $repo = new BitBucketRemoteRepository('r/r', './');
+        $this->assertEquals('https://bitbucket.org/r/r/src/v2/src/Project.php#Project.php-30', $repo->getFileUrl('v2', 'src/Project.php', 30));
+        $this->assertEquals('src/Project.php', $repo->getRelativePath('./src/Project.php'));
+    }
+
+    /**
+     * Test for GitHubRemoteRepository class
+     */
+    public function testGitHubRemoteRepository(): void
+    {
+        $repo = new GitHubRemoteRepository('r/r', './');
+        $this->assertEquals('https://github.com/r/r/blob/v2/src/Project.php#L30', $repo->getFileUrl('v2', 'src/Project.php', 30));
+        $this->assertEquals('src/Project.php', $repo->getRelativePath('./src/Project.php'));
+    }
+
+    /**
+     * Test for GitLabRemoteRepository class
+     */
+    public function testGitLabRemoteRepository(): void
+    {
+        $repo = new GitLabRemoteRepository('r/r', './');
+        $this->assertEquals('https://gitlab.com/r/r/blob/v2/src/Project.php#L30', $repo->getFileUrl('v2', 'src/Project.php', 30));
+        $this->assertEquals('src/Project.php', $repo->getRelativePath('./src/Project.php'));
+    }
+}
