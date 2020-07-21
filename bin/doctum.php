326016812021ff3130d502b0d14e819baed00f31
William Desportes <williamdes@wdes.fr>
2020-07-21 20:59:23 +0200

    feat: Add an error for PHP versions below 7.1

--- bin/doctum.php
+++ bin/doctum.php
@@ -1,6 +1,14 @@
 #!/usr/bin/env php
 <?php
 
+if (PHP_VERSION_ID <= 70100) {
+    echo 'You need to use PHP 7.1 to run Doctum.' . PHP_EOL;
+    $majorVersion = (int) (PHP_VERSION_ID / 10000);
+    $minorVersion = (int) substr((string) PHP_VERSION_ID, -4, 2);
+    echo 'Current detected version: (' . $majorVersion . '.' . $minorVersion . ') (' . PHP_VERSION_ID . ').' . PHP_EOL;
+    exit(1);
+}
+
 // installed via composer?
 $composerDir = __DIR__ . '/../../../autoload.php';
 if (file_exists($composerDir)) {
