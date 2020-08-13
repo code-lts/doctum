1cfd0c7b09c8fcb304b1a731cf0883ec9380cea3
William Desportes <williamdes@wdes.fr>
2020-08-14 00:13:11 +0200

    feat: add commit infos to PHAR update/release script

--- scripts/update-release.sh
+++ scripts/update-release.sh
@@ -7,24 +7,25 @@
 set +e
 
 VERSION="$(cat ./build/VERSION)"
+PHAR_COMMIT="$(git rev-parse --verify HEAD)"
 
 git checkout gh-pages
 
 rm -rf releases/latest
 mv build ./releases/latest
 git add -A ./releases/latest/*
-git commit -S -m "Update latest release" -m "version: ${VERSION}" -m "#latest"
+git commit -S -m "Update latest release" -m "version: ${VERSION}" -m "#latest" -m "Commit: ${PHAR_COMMIT}"
 
 if [ ! -d ./releases/${VERSION}/ ]; then
     mkdir ./releases/${VERSION}/
     cp -rp ./releases/latest/* ./releases/${VERSION}/
     git add -A ./releases/${VERSION}/
-    git commit -S -m "Release ${VERSION}" -m "version: ${VERSION}"
+    git commit -S -m "Release ${VERSION}" -m "version: ${VERSION}" -m "Commit: ${PHAR_COMMIT}"
 else
     rm -rf ./releases/${VERSION}/*
     cp -rp ./releases/latest/* ./releases/${VERSION}/
     git add -A ./releases/${VERSION}/
-    git commit -S -m "Update version ${VERSION}" -m "version: ${VERSION}"
+    git commit -S -m "Update version ${VERSION}" -m "version: ${VERSION}" -m "Commit: ${PHAR_COMMIT}"
 fi
 
 rm -rf build/*
