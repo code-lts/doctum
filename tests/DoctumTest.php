de16fbeec174d61c29be0dc11fd294dcaa56bb56
William Desportes <williamdes@wdes.fr>
2020-11-30 21:51:30 +0100

    tests: Add one more test for array access from outside

--- tests/DoctumTest.php
+++ tests/DoctumTest.php
@@ -8,6 +8,7 @@ use Doctum\Project;
 use Symfony\Component\Finder\Finder;
 use Doctum\Version\GitVersionCollection;
 use Doctum\RemoteRepository\GitHubRemoteRepository;
+use Doctum\Store\JsonStore;
 use Doctum\Version\Version;
 
 /**
@@ -103,5 +104,44 @@ class DoctumTest extends TestCase
                 'blob-blob-says-the-fish'
             )
         ], $project->getVersions());
+    }
+
+    public function testAdvancedHackyIntegration(): void
+    {
+        $iterator = Finder::create()
+                ->files()
+                ->name('*.php')
+                ->exclude('stubs')
+                ->exclude('database')
+                ->exclude('bootstrap')
+                ->exclude('storage')
+                ->in($dir = __DIR__);
+
+        $doctum = new Doctum($iterator, [
+                'title' => 'API',
+                'version' => 'blob-blob-says-the-fish',
+                'build_dir' => __DIR__ . '/build/%version%',
+                'cache_dir' => __DIR__ . '/cache/%version%',
+                'default_opened_level' => 2,
+                'remote_repository' => new GitHubRemoteRepository('RepoName', dirname($dir)),
+        ]);
+        $jsonStore1 = new JsonStore();
+        $jsonStore2 = new JsonStore();
+        unset($doctum['store']);
+        $doctum['store'] = function () use (&$jsonStore1) {
+            return $jsonStore1;
+        };
+        $project = $doctum->getProject();
+        $this->assertInstanceOf(Project::class, $project);
+        $this->assertEquals([
+            new Version(
+                'blob-blob-says-the-fish',
+                'blob-blob-says-the-fish'
+            )
+        ], $project->getVersions());
+        // Check this did work as expected by checking references
+        $this->assertTrue(isset($doctum['store']));
+        $this->assertSame($jsonStore1, $doctum['store']);
+        $this->assertNotSame($jsonStore2, $doctum['store']);
     }
 }
