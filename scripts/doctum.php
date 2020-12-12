4467296d639bc7b87989603117e226e31107c508
William Desportes <williamdes@wdes.fr>
2020-12-12 20:20:16 +0100

    ci: add a Doctum parse workflow

---
+++ scripts/doctum.php
@@ -0,0 +1,18 @@
+<?php
+
+use Doctum\Doctum;
+use Doctum\RemoteRepository\GitHubRemoteRepository;
+use Symfony\Component\Finder\Finder;
+
+$dir = realpath(__DIR__ . '/../src');
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in($dir);
+
+return new Doctum($iterator, [
+    'title'                => 'Doctum',
+    'build_dir'            => '/tmp/doctum-build',
+    'cache_dir'            => '/tmp/doctum-cache',
+    'remote_repository'    => new GitHubRemoteRepository('code-lts/doctum', realpath($dir . '/../')),
+]);
