ed1796291f61d95f085390cfcdc06603a1368df9
William Desportes <williamdes@wdes.fr>
2021-03-26 12:25:39 +0100

    tests: update PATH in test step

--- tests/phar/install-tests.sh
+++ tests/phar/install-tests.sh
@@ -6,5 +6,5 @@ if [ ! -f ~/v2.1.8.tar.gz ]; then
     wget -O ~/v2.1.8.tar.gz https://github.com/kward/shunit2/archive/refs/tags/v2.1.8.tar.gz;
 fi
 tar -xvf ~/v2.1.8.tar.gz
-sudo mv ./shunit2-2.1.8 ~/shunit2-2.1.8
-export PATH="$PATH:~/shunit2-2.1.8/"
+mv ./shunit2-2.1.8 ~/shunit2-2.1.8
+chmod +x ~/shunit2-2.1.8/shunit2
