8573eadcaed2ba4e4018d4f6bf4c0c73af02ee94
William Desportes <williamdes@wdes.fr>
2021-04-07 23:07:32 +0200

    tests: add tests for resume from parse

---
+++ tests/resume-from-parse-test.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -e
+
+ROOT="$(realpath "$(dirname $0)/../")"
+
+echo "Running in: ${ROOT}"
+
+cleanWorkspace() {
+    rm -rf tests/phar/data/build
+    rm -rf tests/phar/data/cache
+}
+
+cleanWorkspace
+
+TEMP_FOLDER="$(mktemp -d /tmp/doctum-resume-parse-test.XXXXXXXXX)"
+
+echo "Using temp foler: ${TEMP_FOLDER}"
+
+echo "Running parse"
+./bin/doctum.php parse -v --no-progress --no-ansi --force tests/phar/data/doctum-absolute.conf.php
+
+echo "Running render"
+./bin/doctum.php render -v --no-progress --no-ansi --force tests/phar/data/doctum-absolute.conf.php
+
+echo "Moving files to: ${TEMP_FOLDER}"
+
+mv tests/phar/data/build ${TEMP_FOLDER}
+mv tests/phar/data/cache ${TEMP_FOLDER}
+
+cleanWorkspace
+
+echo "Running update"
+./bin/doctum.php update -v --no-progress --no-ansi --force tests/phar/data/doctum-absolute.conf.php
+
+echo "Comparing cache"
+diff --recursive "${TEMP_FOLDER}/cache/" tests/phar/data/cache/
+if [ $? -eq 0 ]; then
+    echo "The cache directories where the same"
+fi
+
+echo "Comparing build"
+diff --recursive "${TEMP_FOLDER}/build/" tests/phar/data/build/
+if [ $? -eq 0 ]; then
+    echo "The build directories where the same"
+fi
