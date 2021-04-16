e398f9733d7952a7015e3ee91c54b90184b928c9
William Desportes <williamdes@wdes.fr>
2021-04-16 17:43:26 +0200

    feat: add tests and types for GitVersionCollection

---
+++ tests/Version/GitVersionCollectionTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace Doctum\Tests\Version;
+
+use Doctum\Tests\AbstractTestCase;
+use Doctum\Version\GitVersionCollection;
+use Doctum\Version\Version;
+
+class GitVersionCollectionTest extends AbstractTestCase
+{
+
+    public function testConstruct(): void
+    {
+        $o = new GitVersionCollection('');
+        $this->assertSame(0, $o->count());
+        $this->assertSame([], $o->getVersions());
+    }
+
+    public function testConstructAddVersion(): void
+    {
+        $o = $this->getMockBuilder(GitVersionCollection::class)
+            ->setMethods(['execute'])
+            ->setConstructorArgs([''])
+            ->getMock();
+        $o->expects($this->exactly(1))
+            ->method('execute')
+            ->withConsecutive(
+                [
+                    [
+                        'tag'
+                    ]
+                ]
+            )->willReturnOnConsecutiveCalls(
+                'RC' . "\n"
+                . 'v5.0.0' . "\n"
+                . 'v5.0.1' . "\n"
+                . 'v5.0.2' . "\n"
+                . 'v5.0.3' . "\n"
+                . 'v5.1.0' . "\n"
+                . 'v5.2.0' . "\n"
+                . '' . "\n"
+                . 'ALPHA' . "\n"
+                . 'v5.2.1' . "\n"
+                . 'v5.3.0' . "\n"
+                . 'v5.3.1' . "\n"
+                . 'v5.3.2' . "\n"
+                . 'v5.4.0'
+            );
+        /** @var GitVersionCollection $o */
+        $o->addFromTags();
+        $this->assertSame(11, $o->count());
+        $this->assertEquals(
+            [
+            $this->getVersion('v5.0.0'),
+            $this->getVersion('v5.0.1'),
+            $this->getVersion('v5.0.2'),
+            $this->getVersion('v5.0.3'),
+            $this->getVersion('v5.1.0'),
+            $this->getVersion('v5.2.0'),
+            $this->getVersion('v5.2.1'),
+            $this->getVersion('v5.3.0'),
+            $this->getVersion('v5.3.1'),
+            $this->getVersion('v5.3.2'),
+            $this->getVersion('v5.4.0'),
+            ],
+            $o->getVersions()
+        );
+    }
+
+    public function testConstructAddVersionCustomFilter(): void
+    {
+        $o = $this->getMockBuilder(GitVersionCollection::class)
+            ->setMethods(['execute'])
+            ->setConstructorArgs([''])
+            ->getMock();
+        $o->expects($this->exactly(1))
+            ->method('execute')
+            ->withConsecutive(
+                [
+                    [
+                        'tag'
+                    ]
+                ]
+            )->willReturnOnConsecutiveCalls(
+                'RC' . "\n"
+                . 'v5.0.0' . "\n"
+                . 'v5.0.1' . "\n"
+                . 'v5.0.2' . "\n"
+                . 'v5.0.3' . "\n"
+                . 'v5.1.0' . "\n"
+                . 'v5.2.0' . "\n"
+                . '' . "\n"
+                . 'ALPHA' . "\n"
+                . 'v5.2.1' . "\n"
+                . 'v5.3.0' . "\n"
+                . 'v5.3.1' . "\n"
+                . 'v5.3.2' . "\n"
+                . 'v5.4.0'
+            );
+        /** @var GitVersionCollection $o */
+        $o->setFilter(
+            static function (string $version): bool {
+                return ! in_array($version, ['v5.0.0', 'v5.0.1', 'v5.0.2', 'v5.0.3', 'v5.1.0', 'v5.2.0']);
+            }
+        );
+        $o->addFromTags();
+        $this->assertSame(7, $o->count());
+        $this->assertEquals(
+            [
+            $this->getVersion('ALPHA'),
+            $this->getVersion('v5.2.1'),
+            $this->getVersion('v5.3.0'),
+            $this->getVersion('v5.3.1'),
+            $this->getVersion('v5.3.2'),
+            $this->getVersion('v5.4.0'),
+            $this->getVersion('RC'),
+            ],
+            $o->getVersions()
+        );
+    }
+
+    protected function getVersion(string $name): Version
+    {
+        $a = new Version($name);
+        $a->setFrozen(true);
+        return $a;
+    }
+
+}
