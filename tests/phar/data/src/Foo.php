f1db434d22cb4a9e3971e06794a64e18fcd70c0e
William Desportes <williamdes@wdes.fr>
2021-04-21 14:37:43 +0200

    tests: update test data

--- tests/phar/data/src/Foo.php
+++ tests/phar/data/src/Foo.php
@@ -7,10 +7,92 @@ use Closure;
 class Foo
 {
     /**
-     * @param Closure(string...):void $fn
+     * Kwa
+     *
+     * level 1
+     * --------
+     *
+     * level 1
+     * ----------
+     * level 1
+     * ------------
+     *
+     *Describe {@link https://doctum.long-term.support } is LTS
+     * Describe {@link Foo::expects_void_variadic } function
+     * Describe {@link expects_void_variadic } function
+     *
+     * Describe2 { @link https://doctum.long-term.support } is LTS
+     * Describe2 { @link Foo::expects_void_variadic } function
+     * Describe2 { @link expects_void_variadic } function
+     *
+     * 
+     * @see expects_void_variadic
+     * @see https://doctum.long-term.support
+     * @see Foo::expects_void_variadic
+     *
+     * You need to {@see expects_void_variadic}
+     * You need to {@see https://doctum.long-term.support}
+     * You need to {@see Foo::expects_void_variadic}
+     *
+     * 
      * @return void
      */
     function expects_void_variadic(Closure $fn): void {
+    }
+
+    /**
+      * Text with an inline source tag:
+      *
+      * <code>
+      * function element($pages)
+      * {
+      *     if (empty($pages))
+      *     {
+      *         die("<b>ERROR</b>: nothing parsed");
+      *     }
+      * }
+      * </code>
+      * displays without a break in the flow
+      */
+    function element($pages)
+    {
+
+        if (empty($pages))
+        {
+            die("<b>ERROR</b>: nothing parsed");
+        }
+
+    }
+
+    /**
+     * A
+     * B
+     * C
+     */
+    function md(): string
+    {
+
+    }
+
+    /**
+     * # A
+     * ## B
+     * ### C
+     */
+    function md2(): string
+    {
+
+    }
+
+    /**
+     * # A
+     * ## B
+     * ### C
+     * @return foo|bar|string|mixed|false|true|int|foo|bar|string|mixed|false|true|int
+     */
+    function longSignature()
+    {
+
     }
 }
 $f = new Foo();
