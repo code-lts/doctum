8a642d072b1b8660f8ac8b0ec5c1fd9cb76283fd
William Desportes <williamdes@wdes.fr>
2022-11-18 15:11:21 +0100

    Ref #52 - Add examples that have `@example` tags into them and use multiple lines

--- tests/phar/data/src/StringExample.php
+++ tests/phar/data/src/StringExample.php
@@ -9,6 +9,18 @@ class IPv4Address implements Stringable {
     private string $oct3;
     private string $oct4;
 
+    /**
+     * Elementum semper nullam primis tempus quisque tempor.
+     *
+     * @example
+     * $redis->pipeline()
+     *       ->select(1)
+     *       ->del('newkey')
+     *       ->select(0)
+     *       ->del('newkey')
+     *       ->mset(['source1' => 'value1', 'exists' => 'old_value'])
+     *       ->exec();
+     */
     public function __construct(string $oct1, string $oct2, string $oct3, string $oct4) {
         $this->oct1 = $oct1;
         $this->oct2 = $oct2;
@@ -16,6 +28,12 @@ class IPv4Address implements Stringable {
         $this->oct4 = $oct4;
     }
 
+    /**
+     * Elementum semper nullam primis tempus quisque tempor.
+     *
+     * @example $redis->copy('source1', 'newkey');
+     * @example $redis->copy('source1', 'newkey', ['db' => 1]);
+     */
     public function __toString(): string {
         return "$this->oct1.$this->oct2.$this->oct3.$this->oct4";
     }
