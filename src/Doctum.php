a6244c50815aecd947aa1a3211f98e10a9ab61f9
William Desportes <williamdes@wdes.fr>
2023-03-12 14:10:19 +0100

    chore: start 5.5.2-dev

--- src/Doctum.php
+++ src/Doctum.php
@@ -51,8 +51,8 @@ use Wdes\phpI18nL10n\Launcher;
 class Doctum implements ArrayAccess
 {
     public const VERSION_MAJOR = 5;
-    public const VERSION_MINOR = 6;
-    public const VERSION_PATCH = 0;
+    public const VERSION_MINOR = 5;
+    public const VERSION_PATCH = 2;
     public const IS_DEV        = true;
 
     //@phpstan-ignore-next-line
